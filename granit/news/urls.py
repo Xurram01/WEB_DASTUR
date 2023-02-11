7ae61dfebb286dbb1ccc086edb619bb3210c920e
Xurramshoh <x.asalboyev1@gmail.com>
2023-02-12 01:21:18 +0500

    add

--- granit/news/urls.py
+++ granit/news/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('about', views.about, name='about')
+    path('', views.news_home, name='news_home')
 ]
--- granit/news/views.py
+++ granit/news/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def news_home(requests):
+    return render(requests, 'main/about.html')
