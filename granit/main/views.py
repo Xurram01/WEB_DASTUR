78186e617f5dd123540cf5ed8ee5b0cca0384ecb
Xurramshoh <x.asalboyev1@gmail.com>
2023-02-11 20:08:14 +0500

    add

--- granit/main/views.py
+++ granit/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return render(request, '')
+    return render(request, 'main/index.html')
 def about(request):
-    return HttpResponse("<h4>YA i ti</h4>")
+    return render(request, 'main/about.html')
