3a22b5016ac45276004aebb4cc7019fce87f2d80
Xurramshoh <x.asalboyev1@gmail.com>
2023-02-12 14:06:09 +0500

    add

--- granit/news/models.py
+++ granit/news/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Artiles(models.Model):
+class Articles(models.Model):
     title = models.CharField('Название', max_length=50)
     anons = models.CharField('Анонс', max_length=250)
     full_text = models.TextField('Статья')
@@ -8,4 +8,8 @@ class Artiles(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
 
--- granit/news/views.py
+++ granit/news/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Articles
 
-def news_home(requests):
-    return render(requests, 'news/news_home.html')
+def news_home(request):
+    news = Articles.objects.order_by('-date')
+    return render(request, 'news/news_home.html', {'news': news})
+ def create(request):
+     return render(request, 'news/create.html')
