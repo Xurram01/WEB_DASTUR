e5edd29354fe10df19d2828b9cf466207dc1fcc8
Xurramshoh <x.asalboyev1@gmail.com>
2023-02-12 14:08:39 +0500

    add

--- granit/news/views.py
+++ granit/news/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from .models import Articles
 
-def news_home(request):
+def news_home(requests):
     news = Articles.objects.order_by('-date')
-    return render(request, 'news/news_home.html', {'news': news})
+    return render(requests, 'news/news_home.html', {'news': news})
  def create(request):
      return render(request, 'news/create.html')
